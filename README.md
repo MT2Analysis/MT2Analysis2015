a1755f4e226e64cf0117680d574ff51dda141492
Maria Giulia Ratti <maria.giulia.ratti@cern.ch>
2018-06-29 17:19:49 +0200

    update to cutflow and proto-README

---
+++ README.md
@@ -0,0 +1,34 @@
+### This README is work-in-progress
+
+## Setup and Installation
+### MT2Analysis2015 must work within the cmsenv, which is set through a valid release of CMSSW
+cmsrel CMSSW_8_0_12 # later versions are not tested
+cd CMSSW_8_0_12/src
+cmsenv
+### You can now do the following steps from any directory you like
+### Add a "link" to the remote repository where the code is
+git remote add ana-mt2 https://github.com/MT2Analysis/MT2Analysis2015.git
+### Then clone it locally from the remote
+git clone -o ana-mt2 -b mg-data2017 https://github.com/MT2Analysis/MT2Analysis2015.git myMT2Analysis
+
+### After installation, every time you log in 
+cd CMSSW_8_0_12/src
+cmsenv
+
+## Compilation
+make <name-of-file-you-want-to-compile>
+### Note: everytime you change interface/mt2.h you have to:
+make clean 
+# and then recompile
+
+# Run - time !
+## Control Region Plots
+### First you need to run the estimates 
+./regionEventYields <cfg-file-name>
+./llepControlRegion <cfg-file-name> 
+./zllControlRegion <cfg-file-name> 
+### Then you can make the Data/MC plots
+./drawLostLeptonControlRegionDataMC <cfg-file-name> <lumi/shape>
+./drawZllControlRegion <cfg-file-name> <lumi/shape>
+
+### Next step: run the background estimation !
--- scripts/cutflow.py
+++ scripts/cutflow.py
@@ -1,13 +1,13 @@
 # Script to write the cutflow for several regions of the MT2 analysis
-# specigy the sample name of the babytree, the year of data-taking, and the region where you want the cutflow
+# specify the sample name of the babytree, the year of data-taking, and the region where you want the cutflow
 
 # setup:
 # a recent root version and a python version >=2.7
 
 # For the moment implemented only for SR and CR1lep
-# TODO: implement also for di-lepton and photon region
-# Note: the so-called 'cleanings' are commented out because take forever to run (why?)
-# anyhow the impact is very small
+# TODO: QCD region (and Photon CR)
+# TODO: missing id cut on electrons for zll CR
+# Note: the so-called 'cleanings' are commented out because take forever to run (why?); anyhow the impact is very small
 
 
 from ROOT import TChain, TH1F
@@ -16,6 +16,7 @@ cuts={}
 #cut[<year>][<cutName>] = <cutExpr>
 
 cuts['2016'] = {}
+# cuts of the signal region and single-lepton CR
 cuts['2016']['noCut'] =             '(1)'
 cuts['2016']['isGolden'] =          '(isGolden)'
 cuts['2016']['goodVertex'] =        '(nVert > 0)'
@@ -31,6 +32,17 @@ cuts['2016']['leptonVeto'] =        '(nMuons10==0 && nElectrons10==0)'
 cuts['2016']['isoTrackVeto'] =      '(nPFLep5LowMT==0 && nPFHad10LowMT==0)'
 cuts['2016']['1lepton'] =           '(nLepLowMT==1)' # should be equivalent to '(nMuons10==1 || nElectrons10==1 || nPFLep5LowMT==1 || nPFHad10LowMT==1 )'
 cuts['2016']['incl1leptonCR'] =     '(ht>250. && met_pt>250  && nJet30>1 && mt2>200.)'
+# cuts that are dilepton CR specific
+cuts['2016']['zll_SF_triggers'] =   '( lep_pdgId[0] == -lep_pdgId[1] && (HLT_DoubleMu || HLT_DoubleMu_NonIso || HLT_SingleMu_NonIso || HLT_DoubleEl || HLT_DoubleEl33 || HLT_Photon165_HE10))' 
+cuts['2016']['zll_OF_triggers'] =   '( lep_pdgId[0] != -lep_pdgId[1] && ( HLT_MuX_Ele12 || HLT_Mu8_EleX || HLT_Mu33_Ele33_NonIso || HLT_Mu30_Ele30_NonIso || HLT_Photon165_HE10 || HLT_SingleMu_NonIso ))'
+cuts['2016']['zll_HT_Etmiss'] =     '((nJet30>1 && zll_ht<1000. && zll_met_pt>250.) || ( nJet30>1 && zll_ht>=1000. && zll_met_pt>30.) || (nJet30==1 && zll_met_pt>250.) )'
+cuts['2016']['zll_deltaPhi>0.3'] =  '(zll_deltaPhiMin > 0.3)'
+cuts['2016']['zll_HTmiss-Etmiss'] = '(zll_diffMetMht < 0.5*zll_met_pt)'
+cuts['2016']['zll_MT2'] =           '((((zll_mt2>200 && zll_ht<1500) || (zll_mt2>400 && zll_ht>=1500)) &&(nJet30>1) )  ||  ( zll_ht>200 && nJet30==1 )  )'
+cuts['2016']['ZmassPt'] =           '(zll_pt > 200. && fabs(zll_mass-91.19)<20)'
+cuts['2016']['2leptons'] =          '(nLep==2 && lep_pdgId[0]*lep_pdgId[1])>0 && lep_pt[0]>100 && lep_pt[1]>30 &&) '
+# abs(myTree.lep_pdgId[0])==11 && myTree.lep_tightId[0]< 0.5
+# abs(myTree.lep_pdgId[1])==11 && myTree.lep_tightId[1]< 0.5 
 
 cuts['2017'] = {}
 cuts['2017']['noCut'] =             cuts['2016']['noCut']
@@ -48,11 +60,20 @@ cuts['2017']['isoTrackVeto'] =      cuts['2016']['isoTrackVeto']
 cuts['2017']['MT2'] =               cuts['2016']['MT2']
 cuts['2017']['1lepton'] =           cuts['2016']['1lepton']
 cuts['2017']['incl1leptonCR'] =     cuts['2016']['incl1leptonCR']
+# cuts that are dilepton CR specific
+cuts['2017']['zll_SF_triggers'] =   cuts['2016']['zll_SF_triggers']
+cuts['2017']['zll_OF_triggers'] =   cuts['2016']['zll_OF_triggers']
+cuts['2017']['zll_HT_Etmiss'] =     '((nJet30>1 && zll_ht<1000. && zll_met_pt>250.) || ( nJet30>1 && zll_ht>=1000. && zll_met_pt>30.) || (nJet30==1 && zll_met_pt>250.) )'
+cuts['2017']['zll_deltaPhi>0.3'] =  cuts['2016']['zll_deltaPhi>0.3']
+cuts['2017']['zll_HTmiss-Etmiss'] = cuts['2016']['zll_HTmiss-Etmiss']
+cuts['2017']['zll_MT2'] =           cuts['2016']['zll_MT2']
+cuts['2017']['ZmassPt'] =           cuts['2016']['ZmassPt']
+cuts['2017']['2leptons'] =          cuts['2016']['2leptons']
 
 ordered_cutNames = {}
 ordered_cutNames['SR'] =     ['noCut' ,'isGolden', 'goodVertex', 'filters', 'triggers', 'HT_Etmiss', 'nJets>0', 'deltaPhi>0.3', 'HTmiss-Etmiss', 'MT2', 'leptonVeto','isoTrackVeto', ]
 ordered_cutNames['CR1lep'] = ['noCut' ,'isGolden', 'goodVertex', 'filters', 'triggers', 'HT_Etmiss', 'nJets>0', 'deltaPhi>0.3', 'HTmiss-Etmiss', 'MT2', '1lepton', 'incl1leptonCR']
-#ordered_cutNames['CR2lep'] =
+ordered_cutNames['CR2lep'] = ['noCut' ,'isGolden', 'goodVertex', 'filters', 'zll_SF_triggers', 'zll_HT_Etmiss', 'nJets>0', 'zll_deltaPhi>0.3', 'zll_HTmiss-Etmiss', 'zll_MT2', 'ZmassPt', '2leptons']
 #ordered_cutNames['CR1ph'] =
 #ordered_cutNames['SR'] = ['noCut', 'cleanings']
 
